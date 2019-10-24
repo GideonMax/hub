0602a546b00735a941eab86f1e15ab0672c6b6cf
GideonMax <45212668+GideonMax@users.noreply.github.com>
2019-10-24 18:02:56 +0300

    f

--- info_to_xlsx/maketable.js
+++ info_to_xlsx/maketable.js
@@ -4,23 +4,29 @@ let fs = require("fs")
 
 
 
-function object_to_xlsx(obj,file) {
+function object_to_xlsx(obj,file,optional_headers=false) {
   let workbook = new xl.Workbook()
   let sheet = workbook.addWorksheet("main")
+  console.log(Object.keys(obj));
+  if(optional_headers!=false)
+  {
+    for (var i=0;i< optional_headers.length ;i++) {
+      row.push( optional_headers[i])
+    }
+  }
   for (var d=0;d< Object.keys(obj).length;d++ ) {
     var key = Object.keys(obj)[d]
-    var row = [key]
-    console.log(Object.keys(obj));
+    var row = []
     console.log(key)
-    for (var i=0;i< Object.keys( obj[key]).length ;i++) {
-      var thing = Object.keys( obj[key])[i]
-      row.push( thing + ": "+  obj[key][thing])
+    for (var i=0;i< obj[key].length ;i++) {
+      row.push( obj[key][i])
     }
     sheet.addRow(row)
   }
   workbook.xlsx.writeFile(file)
   return
 }
+
 async function mailfile(filep,filen,sendto,mail,password) {
   let dd ={
     service: 'gmail.com',
@@ -59,3 +65,14 @@ async function mailfile(filep,filen,sendto,mail,password) {
     console.log('Email sent: ' + info.response)
   }
 })}
+
+
+
+var k =
+{
+  headers:['name','school','class','parent\'s phone','warm','cold','weird question'],
+  person_a:['guy','a school','10', '75648','true','true','sushi'],
+  yes:['a','schoooool','3','not a phone number, you have been bumboozled','true','false', 'CAT']
+}
+//object_to_xlsx(k,'C:/Users/Gidi/Documents/GitHub/hub/info_to_xlsx/test.xlsx')
+mailfile('C:/Users/Gidi/Documents/GitHub/hub/info_to_xlsx/','test.xlsx','gideon.max.merling@gmail.com','gideon.max.merling@gmail.com','123re123ewq')
