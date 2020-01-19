32492874f0ca43f1a1dc252087674024f2539f0b
GideonMax <45212668+GideonMax@users.noreply.github.com>
2020-01-19 19:04:42 +0200

    started centralization/bundling of different parts

--- XChart/XChartServer.js
+++ XChart/XChartServer.js
@@ -6,15 +6,6 @@ app.use(bodyPraser.json());
 let ejs = require('ejs')
 const drinks =  require('../js_stats/stats_handler.js');
 app.use(bodyPraser.urlencoded({ extended: true }));
-/*
-var serviceAccount = require(__dirname+ "/fire.json");
-var admin = require("firebase-admin");
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://test-40ac5.firebaseio.com"
-});
-var db = admin.database();
-var ref = db.ref('/');*/
 
 app.get("/XChart.dat",function(req,res){
   var handler = new drinks.stats_handler('../js_stats/statTest')
@@ -39,19 +30,5 @@ app.get("/barmen.dat",function(req,res){
   })
 })
 */
-function day(da)
-{
-    if(da>6||da<0)return null;
-    switch(da)
-    {
-        case 0: return "Sunday"
-        case 1: return "Monday"
-        case 2: return "Tuesday"
-        case 3: return "Wednesday"
-        case 4: return "Thursday"
-        case 5: return "Friday"
-        case 6: return "Saturday"
-    }
-}
 
 app.listen(3000)
---
+++ finalServer.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+var fs = require('fs');
+const bodyPraser = require('body-parser');
+app.use(bodyPraser.json());
+app.use(bodyPraser.urlencoded({ extended: true }));
+const drinks = require('./requires/js_stats/stats_handler.js');
+
+app.get("/:name.wc", (req, res) => {
+    res.sendFile(__dirname + "/WebComponents/" + req.params.name + ".js")
+})
+
+app.get("/:name.css", (req, res) => {
+    res.sendFile(__dirname + "/css/" + req.params.name + ".css");
+})
+
+app.get("/:name.html", (req, res) => {
+    res.sendFile(__dirname + "/html/" + req.params.name + ".html");
+})
+
+app.get("/XChart.dat", function (req, res) {
+    var handler = new drinks.stats_handler('./requires/js_stats/statTest');
+    res.send({ stat_values: handler.stats_ar, stat_names: handler.ser_ar });
+})
+
+app.get("/:name.js", (req, res) => {
+    res.sendFile(__dirname + "/js/" + req.params.name + ".js")
+})
+
+app.get("/:name.load", (req, res) => {
+    res.sendFile(__dirname + "/load/" + req.params.name + ".html");
+})
+
+app.get("/*", (req, res) => {
+    res.status(404)
+})
+
+app.listen(3000)
